35647489b36cda11d67632d8372c32b4c445af07
knyazev-source <dknyazevd11@gmail.com>
2026-05-10 16:58:56 +0300

    Update Mario.py

--- Mario.py
+++ Mario.py
@@ -23,6 +23,7 @@ class Player(Game): # Наибольшая переработка перенос
         self.on_ground = False # Следим касаемся ли платформ
 
     def move(self, platforms): # Добавление в логику доп обьекта
+        global i
         keys = key.get_pressed()
         
         # Горизонтальное движение
@@ -47,6 +48,7 @@ class Player(Game): # Наибольшая переработка перенос
         # Прыжок
         if keys[K_UP] and self.on_ground:
             self.vel_y = -14
+            jump.play()
         # Гравитация
         self.vel_y += 0.5
         self.rect.y += int(self.vel_y)
@@ -63,6 +65,23 @@ class Player(Game): # Наибольшая переработка перенос
                     self.rect.top = plat.rect.bottom
                     self.vel_y = 0
 
+class Monster(Game):
+    def __init__(self, imag, x, y, speed, lx, rx):
+        super().__init__(imag, x, y, speed)
+        self.image = transform.scale(image.load(imag),(35,35))
+        self.lx = int(lx)
+        self.rx = int(rx)
+        self.y = 1
+    def move(self):
+        if self.rect.x <= self.lx:
+            self.y=1
+        elif self.rect.x >= self.rx:
+            self.y=2
+        if self.y==1:
+            self.rect.x += self.speed
+        if self.y==2:
+            self.rect.x -= self.speed
+
 class Coin(sprite.Sprite):
     '''Класс монеток'''
     def __init__(self):
@@ -75,15 +94,57 @@ class Coin(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
-        self.location = ['45-310','440-260','440-400','45-400','440-100','620-90','240-110','620-250','240-210']
+        self.location = ['60-60','440-260','440-400','45-400','440-100','620-90','240-110','620-250','240-210']
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+        if self.rect.colliderect(player.rect):
+            self.loct()
+    def loct(self):
+        if len(self.location) != 0:
+            global h
+            elm = choice(self.location)
+            h = elm
+            self.location.remove(elm)
+            elm = elm.split('-')
+            self.rect.x = int(elm[0])
+            self.rect.y = int(elm[1])
+        else:
+            global dp
+            self.rect.x = -100
+            self.rect.y = -100
+            dp = ['60-60','440-260','440-400','45-400','440-100','620-90','240-110','620-250','240-210','45-310']
+            dp.remove(h)
+            door.loct()
+
+
+
+
+class Exit(sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.color = (247,247,137)
+        self.width = 55
+        self.heigth = 70
+        self.image = transform.scale(image.load('door1.png'),(55,70))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.i = 1
+    def win(self):
+        if self.rect.colliderect(player.rect):
+            global run
+            run = False
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
     def loct(self):
-        elm = choice(self.location)
-        self.location.remove(elm)
+        global dp
+        elm = choice(dp)
+        dp.remove(elm)
         elm = elm.split('-')
-        self.rect.x = int(elm[0])
-        self.rect.y = int(elm[1])
+        self.rect.x = int(elm[0])-35/2
+        self.rect.y = int(elm[1])-25
+
 
 class Wall(sprite.Sprite):
     '''Класс стен'''
@@ -109,39 +170,42 @@ clock = time.Clock()
 
 run = True
 count = 0
-'''Убранно за ненадобностю в классе все
-acl = 0
-a = 0
-q=0
-i = False'''
+counter = 0
+dcounter = 1
+mcounter = 1
+font.init()
+font = font.Font(None, 36)
 
-
-
-'''mixer.init()
+mixer.init()
 mixer.music.load('saundtrek.mp3')
-#mixer.music.play()
+mixer.music.play()
 jump = mixer.Sound('jump.mp3')
 money = mixer.Sound('money.mp3')
 over = mixer.Sound('over.mp3')
-gamo = mixer.Sound('gamo.mp3')'''
+gamo = mixer.Sound('gamo.mp3')
 
 # Создание платформ
 platforms = [] # Делаем препятствия в списке
 earth = Wall(0,450,700,50)
 cell = Wall(0,-1,800,1)
-plat1 = Wall(580,130,100,25)
-plat2 = Wall(400,300,100,25)
-plat3 = Wall(20,100,100,25)
-plat4 = Wall(200,150,100,25)
-plats = Wall(20,350,70,20)
+plat1 = Wall(580,130,100,30)
+plat2 = Wall(400,300,100,30)
+plat3 = Wall(20,100,100,30)
+plat4 = Wall(200,150,100,30)
+plats = Wall(20,350,70,30)
 platforms.extend([earth, cell,plats , plat1, plat2, plat3, plat4]) # Переношу платформы в список
 
 coin = Coin()
 coin.loct()
-coin.location.append('60-60')
+coin.location.append('45-310')
 
-player = Player('Mario.png',30,299,4) # Сменил 3 на 4 вкусовщина
-#prev_rect = player.rect.copy() Отключенно за ненадобностью
+player = Player('Mario.png',30,299,4)
+door = Exit(-100,-100)
+mn1 = Monster('m1_1.png', 315, 415, 2, 35, 630)
+mn2 = Monster('Monster.png', 455, 80, 2, 135, 640)
+
+from time import time
+start_time = time()
 
 while run:
     window.blit(background,(0,0))
@@ -149,99 +213,68 @@ while run:
         keys = key.get_pressed()
         if e.type == QUIT:
             run = False
+        if keys[K_q]:
+            coin.loct()
+
+    now_time = time()
+    timer = 60-int(-1*(start_time - now_time))
+    if timer == 0:
+        break
 
     # Рисуем
     for plat in platforms:
         plat.reset()
 
     # Движение и отрисовка игрока
+    door.reset()
+    door.win()
+
     player.move(platforms)
     player.reset()
     
+    mn1.move()
+    mn1.reset()
+    mn2.move()
+    mn2.reset()
+
     coin.reset()
 
+    if player.rect.colliderect(mn1.rect) or player.rect.colliderect(mn2.rect):
+        mixer.music.stop()
+        over.play()
+        run = False
+
+    timer1 = font.render(str(timer),True,(255,0,0))
+    window.blit(timer1,(10,10))
+
+    counter += 1
+    if counter >= 60:
+        dcounter += 1
+        if dcounter == 1:
+            door.image = transform.scale(image.load('door1.png'),(55,70))
+        elif dcounter == 2:
+            door.image = transform.scale(image.load('door2.png'),(55,70))
+            dcounter = 0
+        mcounter += 1
+        if mcounter == 1:
+            mn1.image = transform.scale(image.load('m1_1.png'),(35,35))
+        elif mcounter == 2:
+            mn1.image = transform.scale(image.load('m1_2.png'),(35,35))
+            mcounter = 0
+        
+        
+        counter = 0
+        
 
     clock.tick(60)
     display.update()
-    #У меня еще огромные планы на этот проект
-'''Логика прыжка в классе
-        if e.type == KEYDOWN and keys[K_UP]:
-            print(0)
-            #когда нажимаю кнопку прышка напишет 0
-            if i:
-                player.rect.y-=10
-                acl = -4.8'''
-
-
-
-'''Частично перенесенно в класс частично переработка
-player.speed = 3
-    if sprite.collide_rect(player, earth):
-        if prev_rect.bottom <= earth.rect.top:
-            player.rect.bottom = earth.rect.top
-            i = True
-            player.speed = 4
-            acl = 0 # ускорение
-            a = 0 # счетчик'''
-        #i = True   Возможный вариант решения проблемы - при прикосновении с любой стороны, но можно будет отпрыгивать от стен(не совсем минус)
 '''
-    if sprite.collide_rect(player, plat1) or sprite.collide_rect(player, plat2) or sprite.collide_rect(player, plat3):
-        if (acl < 0 or acl > 0) and (player.rect.y < plat1.rect.y or player.rect.y < plat2.rect.y or player.rect.y < plat3.rect.y):
-            player.rect.y+=20
-        print(player.rect.x)
-        print(player.rect.y)
-        player.speed = 3
-        acl = 0
-        a = 0
-'''
-
-# Проверка коллайда с синим квадратом
-'''Анологично все либо в классе либо ненадо
-        if player.rect.colliderect(plat2.rect):
-        # Определяем, с какой стороны произошло столкновение
-        if prev_rect.right <= plat2.rect.left:
-            # столкновение справа
-            player.rect.right = plat2.rect.left
-        elif prev_rect.left >= plat2.rect.right:
-            # столкновение слева
-            player.rect.left = plat2.rect.right
-        elif prev_rect.top >= plat2.rect.bottom:
-            # столкновение снизу
-            player.rect.top = plat2.rect.bottom
-            acl = 0
-        elif prev_rect.bottom <= plat2.rect.top:
-            # столкновение сверху
-            player.rect.bottom = plat2.rect.top
-            i = True
-            player.speed = 4
-            acl = 0 # ускорение
-            a = 0 # счетчик
-'''
-
-
-
-
-''' Что то вынесенно в группы чего то ненадо что то просто в классе лежит
-    prev_rect = player.rect.copy()
-
-    earth.reset()
-    plat1.reset()
-    plat2.reset()
-    plat3.reset()
-    player.reset()
-    player.move()
-
-
-
-
-    a+=1
-    if a>=3:
-        i = False
-        player.rect.y+=acl
-        acl += 0.07
-        player.q = 0
-    
-
-
-    clock.tick(60)
+run = True
+while run:
+    window.blit(background,(0,0))
+    for e in event.get():
+        keys = key.get_pressed()
+        if e.type == QUIT:
+            run = False
+    clock.tick(180)
     display.update()'''
